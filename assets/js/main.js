cc46851c996e6a06cac103d5490838f3a598f11b
tung1203 <tung120320@gmail.com>
2020-08-14 17:19:16 +0700

    mega menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  // nav mobile
   $(".nav-bar").click(function () {
     $(".header__right").addClass("active");
     $(".nav-bar--close").addClass("active");
@@ -8,5 +9,16 @@ $(document).ready(function () {
     $(".header__right").removeClass("active");
     $(this).removeClass("active");
     $(".nav-bar").removeClass("de-active");
+  });
+
+  //mega menu
+  $(".mega-menu__content:not(:nth-child(1))").css("display", "none");
+  $(".mega-menu__list li").hover(function () {
+    $(".mega-menu__content").css("display", "none");
+    let currentHover = $(this).attr("data");
+    $(".mega-menu__content[data='" + currentHover + "']").css(
+      "display",
+      "grid"
+    );
   });
 });
